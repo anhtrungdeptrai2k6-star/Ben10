c53a9f8d8df4e6bd79fa9614029e43fbc015b293
anhtrungdeptrai2k6-star <anhtrungdeptrai2k6@gmail.com>
2026-03-02 14:44:21 +0700

    Update vite.config.ts
    Signed-off-by: anhtrungdeptrai2k6-star <anhtrungdeptrai2k6@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig(({mode}) => {
     base: '/ben10/', 
     plugins: [react(), tailwindcss()],
     define: {
+      'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
     resolve: {
